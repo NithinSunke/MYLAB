7aca3c454a6d869f651b62da7745081f0f41a099
Nithin Sunke <39765833+NithinSunke@users.noreply.github.com>
2026-02-13 17:09:36 +0530

    mysql and postgressql terraform

---
+++ terraform/mysql/vars.tf
@@ -0,0 +1,8 @@
+variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaaf2ihiwhrhe4jx7vrsbolhzqipbjn4wgywnbxhhw6c5qn6dcog2wq" }
+variable availability_domain--kWcK-PHX-AD-1 { default = "kWcK:PHX-AD-1" }
+variable fault_domain--FAULT-DOMAIN-1 { default = "FAULT-DOMAIN-1" }
+variable region { default = "us-phoenix-1" }
+variable "admin_username" {}
+variable "admin_password" {}
+variable "db_storage_size" {}
+variable "db_system_name" {}
---
+++ terraform/postgress/vars.tf
@@ -0,0 +1,5 @@
+#variable compartment_ocid { default = "ocid1.compartment.oc1..aaaaaaaawa7byegnc6qbvskvvq4rxvy7hqexa7prc4lah2efjgkyay5sopzq" }
+variable region { default = "us-phoenix-1" }
+variable "db_system_credentials_username" {} 
+variable "db_system_credentials_password_details_password_type" {}
+variable "db_system_credentials_password_details_password" {}
